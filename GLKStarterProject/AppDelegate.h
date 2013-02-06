bc573f2414fe3b3f86bd143b22584237170901ee
Sebastian Buys <sebastian.buys@gmail.com>
2013-02-06 18:23:19 -0500

    Initial Commit

---
+++ GLKStarterProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GLKStarterProject
+//
+//  Created by Sebastian Buys on 2/6/13.
+//  Copyright (c) 2013 Sebastian Buys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GLKStarterProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GLKStarterProject
+//
+//  Created by Sebastian Buys on 2/6/13.
+//  Copyright (c) 2013 Sebastian Buys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
